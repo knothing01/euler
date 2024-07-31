625022fdc29d1a32584fcac4134b53f204e994f3
knothing01 <michael.truzyan@gmail.com>
2024-07-31 21:58:21 +0400

    27

--- 27.py
+++ 27.py
@@ -1,7 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
 def is_prime(n):
     if n <= 1:
         return False
@@ -18,9 +14,39 @@ def is_prime(n):
     
     return True
 
-n = 0
-a = (n ** 2) + n + 41
 
 
-print(is_prime(a))
 
+# formula n^2 + an + b 
+# |a| < 1000 & |b| <= 1000
+# n = 0
+
+def quadratic_primes(a, b):
+    n = 0
+    while True:
+        value = n * n + a * n + b
+        if value <= 0 or not is_prime(value):
+            break
+        n += 1
+
+    return n
+
+def find_max():
+    max_prime = 0
+    a_res = 0
+    b_res = 0
+
+    for a in range (-1000, 1001):
+        for b in range(-1000, 1001):
+            num_prime = quadratic_primes(a, b)
+            if num_prime > max_prime:
+                max_prime = num_prime
+                a_res = a
+                b_res = b
+
+
+    return a_res * b_res
+
+
+result = find_max()
+print(result)
